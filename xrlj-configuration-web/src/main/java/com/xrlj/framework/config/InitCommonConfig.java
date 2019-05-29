4580d532b04483e073ce76858b9026461c4a2d70
xinxiamu <932852117@qq.com>
2019-05-29 14:51:05 +0800

    重构

--- xrlj-configuration-web/src/main/java/com/xrlj/framework/config/InitCommonConfig.java
+++ xrlj-configuration-web/src/main/java/com/xrlj/framework/config/InitCommonConfig.java
@@ -1,17 +1,31 @@
 package com.xrlj.framework.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+
+import java.io.File;
 
 @Slf4j
 @Configuration
 @Order(-1)
 public class InitCommonConfig implements CommandLineRunner {
 
+    @Value("${spring.application.name}")
+    protected String appName;
+
     @Override
     public void run(String... args) throws Exception {
-        log.info(">>>>>服务启动成功：{}",args);
+        String tmpDir = FileUtils.getUserDirectoryPath().concat(File.separator).concat(".xrlj");
+        File file = new File(tmpDir.concat(File.separator).concat(appName));
+        if (!file.exists()) {
+            file.mkdirs();
+            log.info("成功创建应用程序文件目录：{}", file.getAbsolutePath());
+        } else {
+            log.info("应用程序文件目录已存在：{}", file.getAbsolutePath());
+        }
     }
 }
