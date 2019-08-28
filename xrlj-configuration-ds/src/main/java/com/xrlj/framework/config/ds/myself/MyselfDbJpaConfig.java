ac1bfd842b91aec168f48c12e0b568b5be0c38f4
xinxiamu <932852117@qq.com>
2019-08-28 10:50:48 +0800

    重构

--- xrlj-configuration-ds/src/main/java/com/xrlj/framework/config/ds/myself/MyselfDbJpaConfig.java
+++ xrlj-configuration-ds/src/main/java/com/xrlj/framework/config/ds/myself/MyselfDbJpaConfig.java
@@ -1,6 +1,7 @@
 package com.xrlj.framework.config.ds.myself;
 
 import com.xrlj.framework.config.ds.DynamicDataSource;
+import com.xrlj.framework.dao.base.BaseRepositoryFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
@@ -10,17 +11,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-public abstract class AbstractMyselfDbJpaConfig {
-
-	public abstract String entitiesPackage();
+@EnableTransactionManagement(order = 10)
+@EnableJpaRepositories(value = "${system.project.package-dao}", entityManagerFactoryRef = MyselfDbJpaConfigNames.entityManagerFactoryMyself, transactionManagerRef = MyselfDbJpaConfigNames.transactionManagerMyself, repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class)
+public class MyselfDbJpaConfig {
 
 	@Autowired
-	protected Environment environment;
+	protected Environment env;
 
 	@Autowired
 	@Qualifier("dynamicDataSource")
@@ -28,9 +31,10 @@ public abstract class AbstractMyselfDbJpaConfig {
 
 	@Bean(name = MyselfDbJpaConfigNames.entityManagerFactoryMyself)
 	public LocalContainerEntityManagerFactoryBean entityManagerFactoryMyself(EntityManagerFactoryBuilder builder) {
-		LocalContainerEntityManagerFactoryBean emFactory = builder.dataSource(DataSourceSpyUtils.conversion(environment,dynamicDataSource)).properties(myselfJpaProperties().getProperties())
-				.packages(entitiesPackage()) // 设置数据表对应实体类所在位置
+		LocalContainerEntityManagerFactoryBean emFactory = builder.dataSource(DataSourceSpyUtils.conversion(env,dynamicDataSource)).properties(myselfJpaProperties().getProperties())
+				.packages(env.getProperty("system.project.package-entities")) // 设置数据表对应实体类所在位置
 				.persistenceUnit(MyselfDbJpaConfigNames.unitName).build(); //设置持久化管理工厂别名
+//		moreConfig(emFactory);
 		return emFactory;
 	}
 
