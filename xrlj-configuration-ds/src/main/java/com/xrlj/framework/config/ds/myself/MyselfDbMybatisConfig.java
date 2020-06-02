c599819dcd98b6e39495aa4d90c9999da9157d10
xinxiamu <932852117@qq.com>
2020-06-02 18:59:20 +0800

    好好学习，天天向上

--- xrlj-configuration-ds/src/main/java/com/xrlj/framework/config/ds/myself/MyselfDbMybatisConfig.java
+++ xrlj-configuration-ds/src/main/java/com/xrlj/framework/config/ds/myself/MyselfDbMybatisConfig.java
@@ -7,7 +7,6 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
