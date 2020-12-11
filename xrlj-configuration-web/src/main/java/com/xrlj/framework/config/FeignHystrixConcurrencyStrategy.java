0b06d3b33781b68bd51ad3d48c0ec43510b9f08a
xinxiamu <932852117@qq.com>
2020-12-11 17:47:39 +0800

    好好学习，天天向上

--- xrlj-configuration-web/src/main/java/com/xrlj/framework/config/FeignHystrixConcurrencyStrategy.java
+++ xrlj-configuration-web/src/main/java/com/xrlj/framework/config/FeignHystrixConcurrencyStrategy.java
@@ -24,7 +24,13 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * 自定义Feign的隔离策略;
- * 在转发Feign的请求头的时候，如果开启了Hystrix，Hystrix的默认隔离策略是Thread(线程隔离策略)，因此转发拦截器内是无法获取到请求的请求头信息的，可以修改默认隔离策略为信号量模式：hystrix.command.default.execution.isolation.strategy=SEMAPHORE，这样的话转发线程和请求线程实际上是一个线程，这并不是最好的解决方法，信号量模式也不是官方最为推荐的隔离策略；另一个解决方法就是自定义Hystrix的隔离策略，思路是将现有的并发策略作为新并发策略的成员变量,在新并发策略中，返回现有并发策略的线程池、Queue；将策略加到Spring容器即可；
+ * 在转发Feign的请求头的时候，如果开启了Hystrix，Hystrix的默认隔离策略是Thread(线程隔离策略)，
+ * <br>
+ * 因此转发拦截器内是无法获取到请求的请求头信息的，可以修改默认隔离策略为信号量模式：hystrix.command.default.execution.isolation.strategy=SEMAPHORE，
+ * <br>
+ * 这样的话转发线程和请求线程实际上是一个线程，这并不是最好的解决方法，信号量模式也不是官方最为推荐的隔离策略；
+ * <br>
+ * 另一个解决方法就是自定义Hystrix的隔离策略，思路是将现有的并发策略作为新并发策略的成员变量,在新并发策略中，返回现有并发策略的线程池、Queue；将策略加到Spring容器即可；
  */
 @Component
 class FeignHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy {
